dfd4ada21a9a72606c727d9a2d2639bb4b948517
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-07-01 18:48:19 +0200

    Grid ctor which is using another grid

--- Grid/Grid.cpp
+++ Grid/Grid.cpp
@@ -1,14 +1,122 @@
 #include "Grid.hpp"
 
-Grid::Grid(std::string board = "")
+Grid::Grid(std::string board = "") 
 {
+	mSymbolFirstPlayer = '1';
+	mSymbolSecondPlayer = '2';
+
+	mBoard = board;
 	constexpr int size = 6*7;
 
-	if(board.length() > size) 
+	if(mBoard.length() > size) 
 		throw;
 
-	board.append(size - board.length(), '0');
+	mBoard.append(size - mBoard.length(), '0');
+	
+	updateColsRowsDiags(mBoard);
+}
 
+Grid::Grid(std::string board, char player, unsigned int column)
+{
+	mBoard = board;
+	constexpr int size = 6*7;
+	if(mBoard.length() > size) 
+		throw;
+
+	mBoard.append(size - mBoard.length(), '0');
+	
+	Grid tmp(mBoard);
+	if(tmp.isColumnFull(column))
+		throw;
+
+	//TODO: putStone - ?
+
+	for (unsigned int i = 0; i < 6; ++i) {
+		if (mBoard[i*7 + column] == '0') {
+			mBoard[i*7 + column] = player;
+			break;
+		}
+	}
+	updateColsRowsDiags(mBoard);
+}
+
+bool Grid::hasFirstWon()
+{
+	return checkSequence('1',4);
+}
+
+bool Grid::hasSecondWon()
+{
+
+	return checkSequence('2',4);
+}
+
+bool Grid::checkSequence(char player, std::size_t howMany)
+{
+	return (checkRows(player, howMany) 
+		|| checkCols(player, howMany) 
+		|| checkDiags(player, howMany));
+}
+
+bool Grid::checkRows(char player, std::size_t howMany)
+{
+	std::string winningPattern{};
+	winningPattern.append(howMany, player);
+
+	for (int i=0 ; i < 6 ; ++i) {
+		if(rows[i].find(winningPattern) != std::string::npos) {
+			return true;
+		}
+	}
+	return false;
+}
+bool Grid::checkCols(char player, std::size_t howMany)
+{
+	std::string winningPattern{};
+	winningPattern.append(howMany, player);
+
+	for (int i=0 ; i < 7 ; ++i) {
+		if(cols[i].find(winningPattern) != std::string::npos) {
+			return true;
+		}
+	}
+	return false;
+}
+bool Grid::checkDiags(char player, std::size_t howMany)
+{
+	std::string winningPattern{};
+	winningPattern.append(howMany, player);
+
+	for (int i=0 ; i < 12 ; ++i) {
+		if(diags[i].find(winningPattern) != std::string::npos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void Grid::print()
+{
+	for (int i=5 ; i >= 0 ; --i)
+	{
+		std::cout << i << "...";
+		std::cout << rows[i] << std::endl;
+	}
+	std::cout << std::endl;
+
+	char c = 'A';
+	for (int i=0 ; i < 7 ; ++i)
+	{
+		std::cout << static_cast<char>(c+i) << "...";
+		std::cout << cols[i] << std::endl;
+	}
+	std::cout << std::endl;
+
+
+}
+
+void Grid::updateColsRowsDiags(std::string board)
+{
 	for (int i=0 ; i < 6 ; ++i)
 		rows[i] = board.substr(7*i, 7);	
 
@@ -89,87 +197,24 @@ Grid::Grid(std::string board = "")
 	diags[11].push_back(rows[1][2]);
 	diags[11].push_back(rows[2][1]);
 	diags[11].push_back(rows[3][0]);
-
 }
 
-Grid::Grid(std::string board, char player, char column)
+bool Grid::isColumnFull(unsigned int column)
 {
-
-	Grid tmp(board);
-
+	if (cols[column][5] == '0')
+		return false;
+	else
+		return true;
 }
 
-bool Grid::hasFirstWon()
+unsigned int Grid::columnLevel(unsigned int colNumber)
 {
-	return checkAll('1',4);
-}
-
-bool Grid::hasSecondWon()
-{
-
-	return checkAll('2',4);
-}
-
-bool Grid::checkAll(char player, std::size_t howMany)
-{
-	return (checkRows(player, howMany) 
-		|| checkCols(player, howMany) 
-		|| checkDiags(player, howMany));
-}
-
-bool Grid::checkRows(char player, std::size_t howMany)
-{
-	std::string winningPattern{};
-	winningPattern.append(howMany, player);
-
-	for (int i=0 ; i < 6 ; ++i) {
-		if(rows[i].find(winningPattern) != std::string::npos) {
-			return true;
+	for (int i=5; i>=0; --i) {
+		if (cols[colNumber-1][i] != '0') {
+			return (i+1);
 		}
 	}
-	return false;
-}
-bool Grid::checkCols(char player, std::size_t howMany)
-{
-	std::string winningPattern{};
-	winningPattern.append(howMany, player);
-
-	for (int i=0 ; i < 7 ; ++i) {
-		if(cols[i].find(winningPattern) != std::string::npos) {
-			return true;
-		}
-	}
-	return false;
-}
-bool Grid::checkDiags(char player, std::size_t howMany)
-{
-	std::string winningPattern{};
-	winningPattern.append(howMany, player);
-
-	for (int i=0 ; i < 12 ; ++i) {
-		if(diags[i].find(winningPattern) != std::string::npos) {
-			return true;
-		}
-	}
-	return false;
+	
+	throw;
 }
 
-void Grid::print()
-{
-	for (int i=5 ; i >= 0 ; --i)
-	{
-		std::cout << i << "...";
-		std::cout << rows[i] << std::endl;
-	}
-	std::cout << std::endl;
-
-	char c = 'A';
-	for (int i=0 ; i < 7 ; ++i)
-	{
-		std::cout << static_cast<char>(c+i) << "...";
-		std::cout << cols[i] << std::endl;
-	}
-	std::cout << std::endl;
-
-
-}
--- Grid/Grid.hpp
+++ Grid/Grid.hpp
@@ -11,18 +11,25 @@
 class Grid{
 	public:
 	Grid(std::string board);
-	Grid(std::string board, char player, char column);
+	Grid(std::string board, char player, unsigned int column);
 	bool hasFirstWon();
 	bool hasSecondWon();
+	std::string getBoard();
+	unsigned int columnLevel(unsigned int);
 	
 	void print();
 
 	private:
-	bool checkAll(char, std::size_t);
+	bool checkSequence(char, std::size_t);
 	bool checkRows(char, std::size_t);
 	bool checkCols(char, std::size_t);
 	bool checkDiags(char, std::size_t);
+	void updateColsRowsDiags(std::string board);
+	bool isColumnFull(unsigned int);
 
+	std::string mBoard;
+	char mSymbolFirstPlayer;
+	char mSymbolSecondPlayer;
 	std::string cols[7];
 	std::string rows[6];
 	std::string diags[12];
